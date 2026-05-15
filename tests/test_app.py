2dbaa4d8c0bc5d9a8669741eddc54b38d93720e8
Sparsh Gupta <sparshgupta.2005.2005@gmail.com>
2026-05-15 16:16:31 +0530

    small change

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,7 @@ from app import calculate_total
 
 def test_calculate_total_adds_two_numbers():
     assert calculate_total(2, 3) == 5
+
+
+def test_calculate_total_adds_two_numbers():
+    assert calculate_total(3, 3) == 6  
